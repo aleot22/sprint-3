8a79311a43eacdad9d127caf8f85a83f585ac3d2
aleot22 <164351191+aleot22@users.noreply.github.com>
2024-04-28 22:21:07 +0200

    Delete workspace/sprint2.3 directory

--- workspace/sprint2.3/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'], // Buscar archivos de prueba que terminen en .test.ts en todo el proyecto
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  };
